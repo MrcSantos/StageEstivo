3ab9c4560e46b7aa5de13b6d98aa00a137fd45dd
MrcSossy <scartidirobe@gmail.com>
2018-08-21 12:06:39 +0200

    R. V. 0.0.3

--- App/public/javascripts/main.js
+++ App/public/javascripts/main.js
@@ -1,5 +1,34 @@
-$(function() {
-  $('#calendar').fullCalendar({
-    defaultView: 'month'
-  });
+$(function () {
+	$('#calendar').fullCalendar({
+		defaultView: 'month',
+
+		header: {
+			center: 'addEventButton'
+		},
+
+		customButtons: {
+			addEventButton: {
+				text: 'add event...',
+				click: function () {
+					var dateStr = prompt('Enter a date in YYYY-MM-DD format');
+					var date = moment(dateStr);
+
+					if (date.isValid()) {
+						var title = prompt('Enter a title');
+
+						$('#calendar').fullCalendar('renderEvent', {
+							title: title,
+							start: date,
+							allDay: true
+						});
+						alert('Great. Now, update your database...');
+					} else {
+						alert('Invalid date.');
+					}
+				}
+			}
+		}
+
+
+	});
 });
