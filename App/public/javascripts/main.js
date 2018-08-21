3a15c6de4201e37e2a986fb28a23f835a29dce45
MrcSossy <scartidirobe@gmail.com>
2018-08-21 12:37:27 +0200

    Prima degli eventi selezionabili

--- App/public/javascripts/main.js
+++ App/public/javascripts/main.js
@@ -1,14 +1,22 @@
 $(function () {
 	$('#calendar').fullCalendar({
-		defaultView: 'month',
-
+		defaultView: 'agendaWeek',
+		selectable: true,
+		allDaySlot: false,
+		nowIndicator: true,
+		minTime: '06:00:00',
+		maxTime: '22:00:00',
+		firstDay: 1,
+		
 		header: {
-			center: 'addEventButton'
+			left: 'today prev,next',
+			center: 'title',
+			right: 'addEventButton agendaWeek,agendaDay'
 		},
 
 		customButtons: {
 			addEventButton: {
-				text: 'add event...',
+				text: '+',
 				click: function () {
 					var dateStr = prompt('Enter a date in YYYY-MM-DD format');
 					var date = moment(dateStr);
