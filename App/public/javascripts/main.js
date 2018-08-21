f77afc77d091f940d6ec62edb02acd071d2662e2
MrcSossy <scartidirobe@gmail.com>
2018-08-21 16:42:17 +0200

    R. V. 0.0.5

--- App/public/javascripts/main.js
+++ App/public/javascripts/main.js
@@ -1,9 +1,15 @@
+/**
+ * Global variables:
+ * - selectedStartDate: The selected start date and time
+ * - selectedEndDate: The selected end date and time
+ */
 var selectedStartDate, selectedEndDate;
 
 $(function () {
 	$('#calendar').fullCalendar({
 		defaultView: 'agendaWeek', // Agenda view as default
-		selectable: true,
+		selectable: true, // Makes the calendar selectable
+		eventOverlap:false, // Makes the events not stackable one above the other
 		allDaySlot: false, // Removes the all-day events slot
 		nowIndicator: true, // Shows current date/time indicator
 		firstDay: 1, // Starts from Mon
@@ -23,27 +29,27 @@ $(function () {
 			 * if none are given it will ask for it
 			 */
 			addEventButton: {
-				text: '+',
+				text: '+', // Button text
 
 				click: function () {
 					if (isDateSelected()) { // If a date has been selected
 						makeEvent('Ore lavorate', selectedStartDate, selectedEndDate); // Creates an event on the calendar
 					}
-					else { alert("Nessuna data selezionata") }
+					else { alert("Nessuna data selezionata") } // If a date has NOT been selected
 				}
 			}
 		},
 
-		// Select and unselect event handlers
+		/**
+		 * Select and unselect event handlers
+		 * Sets the two global variable to correct data format when selected
+		 * Resets the two global variable when deselected
+		 */
 		select: function (startDate, endDate) {
-			selectedStartDate = startDate.format();
-			selectedEndDate = endDate.format();
+			setSelected(startDate, endDate);
 		},
 		unselect: function (jsEvent, view) {
-			setTimeout(() => {
-				selectedStartDate = undefined;
-				selectedEndDate = undefined;
-			}, 1000);
+			setTimeout(setSelected(), 1000);
 		}
 	});
 });
@@ -52,10 +58,16 @@ function isDateSelected() { // Checks if a date has been selected
 	return selectedStartDate && selectedEndDate;
 }
 
+function setSelected(start, end) { // Sets the global variables values
+	selectedStartDate = start;
+	selectedEndDate = end;
+}
+
 function makeEvent(title, start, end) { // Creates an event on the calendar
 	$('#calendar').fullCalendar('renderEvent', {
 		title: title,
 		start: start,
-		end: end
+		end: end,
+		editable: true, // Makes the event editable (drag, drop and extend/reduce time)
 	});
 }
