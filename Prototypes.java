975ebb81df8c7976e75322033d912e2efef57134
MrcSossy <scartidirobe@gmail.com>
2018-08-23 14:33:50 +0200

    Primi prototipi per @ptorric

---
+++ Prototypes.java
@@ -0,0 +1,24 @@
+/**
+ * Rappresenta un cluster di ore di lavoro
+ */
+public class Evento {
+	String titolo;
+	int|String inizio, fine;
+	boolean isRemote;
+}
+
+/**
+ * Sovrascrive tutti i record che vanno dall'inizio alla fine della settimana
+ * corrispondente alla data di inizio del primo evento
+ * 
+ * @param eventi: Lista di classe Evento contenenti tutti gli eventi della settimana passati tramite la chiamata al server
+ */
+public static void salva(list<Evento> eventi);
+
+/**
+ * Restituisce una lista (o array) di eventi letti dai record che vanno dalla data di inizio a quella di fine
+ * 
+ * @param inizio: La data e l'orario di inizio
+ * @param fine: La data e l'orario di fine
+ */
+public static list<Evento> leggi(int|String inizio, int|String fine);
